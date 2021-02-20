bcab26d551fa961aaf06d51da8fbf054a5de3c79
Sefa DOĞAN <sefadogan7@gmail.com>
2021-02-20 19:12:41 +0300

    Creating base controller

---
+++ Sefd.Phonebook.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sefd.Phonebook.WebApi.Controllers
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        public readonly IMapper _mapper;
+
+        public BaseController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+    }
+}
