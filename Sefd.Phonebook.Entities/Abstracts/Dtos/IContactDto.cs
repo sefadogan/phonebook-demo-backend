a079bbcec68ea58bb720f26f505bd01487ac8421
Sefa DOĞAN <sefadogan7@gmail.com>
2021-02-20 18:36:47 +0300

    Creating DTOs

---
+++ Sefd.Phonebook.Entities/Abstracts/Dtos/IContactDto.cs
@@ -0,0 +1,15 @@
+﻿using Sefd.Phonebook.Core.Entities;
+using System.Collections.Generic;
+
+namespace Sefd.Phonebook.Entities.Abstracts.Dtos
+{
+    public interface IContactDto : IDto
+    {
+        int ContactInformationId { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string CompanyName { get; set; }
+
+        ICollection<IContactInformationDto> ContactInformations { get; set; }
+    }
+}
---
+++ Sefd.Phonebook.Entities/Abstracts/Dtos/IContactInformationDto.cs
@@ -0,0 +1,13 @@
+﻿using Sefd.Phonebook.Core.Entities;
+
+namespace Sefd.Phonebook.Entities.Abstracts.Dtos
+{
+    public interface IContactInformationDto : IDto
+    {
+        int ContactId { get; set; }
+        string Phone { get; set; }
+        string Email { get; set; }
+
+        IContactDto Contact { get; set; }
+    }
+}
