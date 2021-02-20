561d3e9dbe89ceb2748fbc14ab8c99cad4794ae5
Sefa DOĞAN <sefadogan7@gmail.com>
2021-02-20 19:18:12 +0300

    Formating endpoint urls to lowercase

--- Sefd.Phonebook.WebApi/Startup.cs
+++ Sefd.Phonebook.WebApi/Startup.cs
@@ -36,6 +36,9 @@ namespace Sefd.Phonebook.WebApi
             });
 
             DotNetCoreBusinessModule.Load(services);
+
+            // Formating urls to lowercase
+            services.AddRouting(options => options.LowercaseUrls = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
