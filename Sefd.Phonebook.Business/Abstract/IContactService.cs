36d0e30e87fb4bc235a959b6f5d730a034b0c8da
Sefa DOĞAN <sefadogan7@gmail.com>
2021-02-20 18:38:39 +0300

    Creating contact service/manager

---
+++ Sefd.Phonebook.Business/Abstract/IContactService.cs
@@ -0,0 +1,15 @@
+﻿using Sefd.Phonebook.Core.Business;
+using Sefd.Phonebook.Core.Utils.Results;
+using Sefd.Phonebook.Entities.Abstracts.Dtos;
+using Sefd.Phonebook.Entities.Concretes.Dtos;
+using Sefd.Phonebook.Entities.Concretes.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sefd.Phonebook.Business.Abstract
+{
+    public interface IContactService : IBaseService<Contact, ContactDto>
+    {
+        Task<IDataResult<ICollection<IContactDto>>> GetList();
+    }
+}
---
+++ Sefd.Phonebook.Business/Concrete/ContactManager.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Sefd.Phonebook.Business.Abstract;
+using Sefd.Phonebook.Core.Business;
+using Sefd.Phonebook.Core.Utils.Results;
+using Sefd.Phonebook.DataAccess.Abstract;
+using Sefd.Phonebook.Entities.Abstracts.Dtos;
+using Sefd.Phonebook.Entities.Concretes.Dtos;
+using Sefd.Phonebook.Entities.Concretes.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sefd.Phonebook.Business.Concrete
+{
+    public class ContactManager : BaseService<Contact, ContactDto>, IContactService
+    {
+        private readonly IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal, IMapper mapper) : base(mapper)
+        {
+            _contactDal = contactDal;
+        }
+
+        public async Task<IDataResult<ICollection<IContactDto>>> GetList()
+        {
+            ICollection<IContactDto> contactDtos = new List<IContactDto>();
+
+            var contacts = await _contactDal.ListAsync();
+            foreach (var contact in contacts)
+            {
+                var convertedContacts = _mapper.Map<Contact, ContactDto>(contact);
+                contactDtos.Add(convertedContacts);
+            }
+
+            return new DataResult<ICollection<IContactDto>>(contactDtos, true);
+        }
+    }
+}
