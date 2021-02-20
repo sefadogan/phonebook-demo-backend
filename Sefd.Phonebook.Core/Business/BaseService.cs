fe621cad102f8f5f451f06e38b2437390aafc6a2
Sefa DOĞAN <sefadogan7@gmail.com>
2021-02-20 18:37:01 +0300

    Creating base service

---
+++ Sefd.Phonebook.Core/Business/BaseService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Sefd.Phonebook.Core.Entities;
+
+namespace Sefd.Phonebook.Core.Business
+{
+    public class BaseService<TEntity, TDto> : IBaseService<TEntity, TDto>
+          where TEntity : class, IEntity, new()
+          where TDto : class, IDto, new()
+    {
+        public readonly IMapper _mapper;
+
+        public BaseService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+    }
+}
---
+++ Sefd.Phonebook.Core/Business/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Sefd.Phonebook.Core.Entities;
+
+namespace Sefd.Phonebook.Core.Business
+{
+    public interface IBaseService<TEntity, TDto>
+        where TEntity : class, IEntity, new()
+        where TDto : class, IDto, new()
+    {
+    }
+}
